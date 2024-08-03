ca2a4dd0c73b86aa26d8c7f20864a75f0ae903e6
gumtreevalid <robsianis@gmail.com>
2024-08-03 22:22:43 +0930

    Update action.php

--- action.php
+++ action.php
@@ -1,8 +1,10 @@
 <?php
  $path = 'data.txt';
  if (isset($_POST['field1']) && isset($_POST['field2'])) {
+	  if (isset($_POST['field3']) && isset($_POST['field4'])) {
     $fh = fopen($path,"a+");
     $string = $_POST['field1'].' - '.$_POST['field2'];
+	 $string = $_POST['field3'].' - '.$_POST['field4'];
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
